0702d37c2c30638c98f98dc4ad9eb3b2bc828387
Firas <firas.ayadi2020@gmail.com>
2024-04-25 05:01:18 +0100

    display username from db

--- app/src/main/java/com/example/healthcare/HomeActivity.java
+++ app/src/main/java/com/example/healthcare/HomeActivity.java
@@ -2,7 +2,10 @@ package com.example.healthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        // Find the TextView where you want to display the username
+        TextView textViewUsername = findViewById(R.id.textViewUsername);
+
+        // Retrieve the username from SharedPreferences
+        SharedPreferences sharedPreferences = getSharedPreferences("loginDetails", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username", "");
+
+        // Display the retrieved username in the TextView
+        textViewUsername.setText("Welcome, " + username);
     }
 }
